671cc90bb96ed02a7304e84c8206aeb5c5accff8
wangjitaogasdfgsadf123 <wangjitao@rongzi.com>
2017-06-13 17:30:05 +0800

    微调

--- app/src/main/java/walden/com/javadesignmode/activity/ProxyActivity.java
+++ app/src/main/java/walden/com/javadesignmode/activity/ProxyActivity.java
@@ -14,8 +14,10 @@ import walden.com.javadesignmode.mode.proxy.DogPlayGame;
 public class ProxyActivity extends AppCompatActivity {
     @InjectView(R.id.proxy_tv1)
     TextView proxy_tv1;
- @InjectView(R.id.proxy_description)
+    @InjectView(R.id.proxy_description)
     TextView proxy_description;
+    private AIPlayGame ai;
+    private DogPlayGame dog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +33,12 @@ public class ProxyActivity extends AppCompatActivity {
 
     @OnClick(R.id.proxy_bt1)
     void proxy_bt1() {
-        AIPlayGame ai = new AIPlayGame();
-        DogPlayGame dog = new DogPlayGame(ai);
+        if (ai == null) {
+            ai = new AIPlayGame();
+        }
+        if (dog == null) {
+            dog = new DogPlayGame(ai);
+        }
         proxy_tv1.setText(dog.play());
     }
 }
